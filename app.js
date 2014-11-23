4c1809745f1990e7f2b57283a3885ab2dc53b7d3
tknemuru <kojikoji.max.5252@gmail.com>
2014-11-24 03:06:36 +0900

    delete

--- app.js
+++
@@ -1,6 +0,0 @@
-/// <reference path="./Scripts/typings/angularjs/angular.d.ts" />
-"use strict";
-angular.module('app', []).config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/', { templateUrl: '/index.html', controller: 'app.FooController' });
-}]);
-//# sourceMappingURL=app.js.map
