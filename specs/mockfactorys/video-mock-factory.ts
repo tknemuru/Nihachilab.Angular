9b650bae0bcdbf452f741c0a2b79d34b5ced131b
tknemuru <kojikoji.max.5252@gmail.com>
2014-11-24 02:14:42 +0900

    APIから動画データをとってこれるところまで実装。

---
+++ specs/mockfactorys/video-mock-factory.ts
@@ -0,0 +1,43 @@
+﻿/// <reference path="../../scripts/typings/angularjs/angular.d.ts" />
+
+module Nihachilab.MockFactorys {
+    "use strict";
+
+    export class VideoMockFactory {
+        /**
+         * 動画モックリストの要素数を取得します。
+         */
+        public getMockCount(): number {
+            return this.mockCount;
+        }
+        private mockCount: number = 2;
+
+        /**
+         * コンストラクタ
+         */
+        constructor($httpBackend: ng.IHttpBackendService) {
+            $httpBackend.whenGET(Configs.ApiConfig.getVideoUrl())
+                .respond(this.getVideoMocks());
+        }
+
+        /**
+         * 動画モックのリストを取得します。
+         */
+        public getVideoMocks(): Nihachilab.Models.Video[] {
+            var mockVideos: Nihachilab.Models.Video[] = [];
+            for (var i = 0; i < this.mockCount; i++) {
+                mockVideos.push(this.getNewInstance(i));
+            }
+            return mockVideos;
+        }
+
+        /**
+         * 動画モックの新しいインスタンスを取得します。
+         */
+        private getNewInstance(id: number): Nihachilab.Models.Video {
+            var video = new Nihachilab.Models.Video();
+            video.id = id;
+            return video;
+        }
+    }
+}
---
+++ specs/services/video-service-spec.ts
@@ -0,0 +1,29 @@
+﻿/// <reference path="../../scripts/typings/angularjs/angular.d.ts" />
+/// <reference path="../../scripts/typings/angularjs/angular-scenario.d.ts" />
+
+'use strict';
+
+describe('動画サービスのテスト', () => {
+    var $httpBackend: ng.IHttpBackendService;
+    var $http: ng.IHttpService;
+    var videoService: Nihachilab.Services.VideoService;
+    var factory: Nihachilab.MockFactorys.VideoMockFactory;
+
+    beforeEach(inject((_$httpBackend_: ng.IHttpBackendService, _$http_: ng.IHttpService) => {
+        $httpBackend = _$httpBackend_;
+        $http = _$http_;
+
+        // $httpBackendの設定
+        factory = new Nihachilab.MockFactorys.VideoMockFactory($httpBackend);
+
+        // 動画サービスの生成
+        videoService = new Nihachilab.Services.VideoService($http);
+    }));
+
+    it('動画リストを全件取得する', () => {
+        videoService.get((data, status, headers, config) => {
+            expect(data.length).toBe(factory.getMockCount());
+        });
+        $httpBackend.flush();
+    });
+});
